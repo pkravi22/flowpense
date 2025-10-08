896e43c4279d125a1791ff11edd8c94669b90b11
Pramendra Singh <pramendrasinghravi@gmail.com>
2025-10-08 16:11:40 +0530

    fixing bigs

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,21 @@ import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 export default function Home() {
   const router = useRouter();
+const token = useLocalStorage("token", null);
+useEffect(() => {
+  // get token from local storage
 
-  useEffect(() => {
-    const token = useLocalStorage("token", null); // get token from local storage
-
-    if (token) {
-      // decode token to get role (optional)
-      //const payload = JSON.parse(atob(token.split(".")[1]));
-      const payload = "admin";
-      const destination =
-        payload === "admin" ? "/admin/dashboard" : "/user/dashboard";
-      router.replace(destination); // redirect to dashboard
-    } else {
-      router.replace("/login"); // redirect to signin if not logged in
-    }
-  }, [router]);
+  if (token) {
+    // decode token to get role (optional)
+    //const payload = JSON.parse(atob(token.split(".")[1]));
+    const payload = "admin";
+    const destination =
+      payload === "admin" ? "/admin/dashboard" : "/user/dashboard";
+    router.replace(destination); // redirect to dashboard
+  } else {
+    router.replace("/login"); // redirect to signin if not logged in
+  }
+}, [router]);
 
   return <div className="flex justify-center items-center">Loading...</div>; // simple placeholder
 }
